aad4c899168b716471d5d4e232301baffa3e59ee
patryk <gryzabawy@o2.pl>
2023-09-25 22:12:15 +0200

    dodanie func.check_(...)

--- func.py
+++ func.py
@@ -1,3 +1,4 @@
+import requests
 import pandas as pd
 from pathlib import Path
 from typing import Any
@@ -27,16 +28,58 @@ def create_conn() -> MySQLConn:
 
 
 def check_notna(value: Any) -> bool:
-    pass
+    """Checks if value is not None"""
+    if value is not None:
+        return True
+    else:
+        return False
 
 
-def check_ulr():
-    pass
+def check_url(url: str) -> bool:
+    """Checks if url exists"""
+    prefix = 'https://'
+    if prefix not in url:
+        url: str = prefix + url
+
+    try:
+        response = requests.get(url)
+    except requests.exceptions.ConnectionError:
+        return False
+    else:
+        if response.status_code == 200:
+            return True
+        else:
+            return False
 
 
-def check_match():
-    pass
+def check_match(values: list[str] | tuple[str]) -> bool:
+    """Checks if values in list/tuple matches"""
+    match: bool = False
+    for i in range(1, len(values)):
+        if values[i-1] == values[i]:
+            match = True
+        else:
+            match = False
+
+    return match
 
 
-def check_exist():
-    pass
+def check_exist(df: pd.DataFrame,
+                query: str,
+                conn: MySQLConn,
+                cols: str | list[str] | tuple[str] | None = None
+                ) -> bool:
+    """Checks if record exists in database"""
+    if cols is None:
+        cols = ['URL', 'NAME', 'LOG']
+
+    db: pd.DataFrame = conn.read_to_df(query, columns=cols)
+    df: pd.DataFrame = df[cols]
+
+    db['join'] = df[cols].agg(''.join, axis=1)
+    df['join'] = df[cols].agg(''.join, axis=1)
+
+    if db['join'].isin([df.loc[0, 'join']])[0]:
+        return True
+    else:
+        return False
